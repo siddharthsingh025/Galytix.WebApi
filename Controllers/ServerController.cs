295e6ed6791db2d85d8bbf8f076986fb4b8ef4d4
siddharthsingh <siddharthsingh25102001@gmail.com>
2023-11-06 15:13:03 +0530

    Implement the calls asynchronously

--- Controllers/ServerController.cs
+++ Controllers/ServerController.cs
@@ -1,13 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc;
 using System.IO;
-using System.Text.Json;
 using System.Linq;
 using System.Collections.Generic;
-
-
 
 namespace Galytix.WebApi.Controllers
 {
@@ -23,12 +19,45 @@ namespace Galytix.WebApi.Controllers
             return Ok("pong");
         }
 
-        private const string FilePath = "Data/gwpByCountry.csv"; 
+        private const string FilePath = "Data/gwpByCountry.csv";
+
+        private async Task<List<GwpData>> ReadCsvDataAsync()
+        {
+            var lines = await System.IO.File.ReadAllLinesAsync(FilePath);
+            var data = new List<GwpData>();
+
+            var headers = lines[0].Split(',');
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var values = lines[i].Split(',');
+
+                for (int j = 4; j < values.Length; j++)
+                {
+                    if (!string.IsNullOrEmpty(values[j]) && decimal.TryParse(values[j], out decimal gwpValue))
+                    {
+                        var gwpData = new GwpData
+                        {
+                            CountryCode = values[0],
+                            LineOfBusiness = values[3],
+                            Year = headers[j],
+                            Gwp = gwpValue
+                        };
+                        data.Add(gwpData);
+                    }
+                    else
+                    {
+                        // loggs
+                        System.Diagnostics.Debug.WriteLine($"Unable to parse {values[j]} at line {i}, column {j}.");
+                    }
+                }
+            }
+            return data;
+        }
 
         [HttpPost("avg")]
-        public ActionResult<Dictionary<string, decimal>> GetAverageGwpByLob([FromBody] GwpRequest request)
+        public async Task<ActionResult<Dictionary<string, decimal>>> GetAverageGwpByLobAsync([FromBody] GwpRequest request)
         {
-            var data = ReadCsvData();
+            var data = await ReadCsvDataAsync();
             var filteredData = data.Where(d => d.CountryCode.ToLower() == request.Country.ToLower() && request.Lob.Contains(d.LineOfBusiness.ToLower()));
             var result = new Dictionary<string, decimal>();
             foreach (var lob in request.Lob)
@@ -37,44 +66,6 @@ namespace Galytix.WebApi.Controllers
             }
             return result;
         }
-
-  private List<GwpData> ReadCsvData()
-{
-    var lines = System.IO.File.ReadAllLines(FilePath);
-    var data = new List<GwpData>();
-
-    var headers = lines[0].Split(',');
-    for (int i = 1; i < lines.Length; i++)
-    {
-        var values = lines[i].Split(',');
-
-        for (int j = 4; j < values.Length; j++)
-        {
-            if (!string.IsNullOrEmpty(values[j]) && decimal.TryParse(values[j], out decimal gwpValue))
-            {
-                var gwpData = new GwpData
-                {
-                    CountryCode = values[0],
-                    LineOfBusiness = values[3],
-                    Year = headers[j],
-                    Gwp = gwpValue
-                };
-                data.Add(gwpData);
-            }
-            else
-            {
-               //logs
-            System.Diagnostics.Debug.WriteLine($"Unable to parse {values[j]} at line {i}, column {j}.");
-        }
-        }
-    }
-    return data;
-}
-
-
-
-
-
     }
 
     public class GwpRequest
@@ -84,16 +75,10 @@ namespace Galytix.WebApi.Controllers
     }
 
     public class GwpData
-{
-    public string CountryCode { get; set; }
-    public string LineOfBusiness { get; set; }
-    public string Year { get; set; } 
-    public decimal Gwp { get; set; }
+    {
+        public string CountryCode { get; set; }
+        public string LineOfBusiness { get; set; }
+        public string Year { get; set; }
+        public decimal Gwp { get; set; }
+    }
 }
-
-    
- }
-
-
-
-
